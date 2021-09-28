cdd22458256c41c3244c7ba79504233023d70e4d
Ban B <dbove54@gmail.com>
2021-09-28 15:48:32 -0400

    +migrate('up') to create tables in db

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import path from "path";
+import { migrate } from "@prisma/cli";
 
 import { companyRouter } from "./routes/companyRouter";
 import { DBAuthenticationError } from "./error/500s";
 import { statusCodedError } from "./error/statusCodedError";
+
+migrate('up');
 
 // -------------------firing express app
 const app = express();
