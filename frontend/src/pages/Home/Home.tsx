303b7b215e467c1724b95ddb5ba72f33abb6b150
Ric Donati <rdonati5645@gmail.com>
2021-10-01 12:39:19 -0400

    Adds example of frontend connecting to backend

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
-import { Typography } from '@mui/material'
+import { useState } from 'react';
+import { Button, Typography } from '@mui/material'
+import axios from 'axios'
 
 export default function Home() {
-  return <Typography variant='h1'>Home</Typography>
+  const [fetchedResult, setFetchedResult] = useState<any>(null);
+  const testRequest = async () => {
+    const res = await axios.get('http://localhost:5000/home')
+    setFetchedResult(res.data)
+  }
+  return (
+    <>
+      <Typography variant='h1'>Home</Typography>
+      <Button onClick={testRequest}>Fetch</Button>
+      <Typography>Fetched result: {JSON.stringify(fetchedResult, null, 2)}</Typography>
+    </>
+  )
 }
