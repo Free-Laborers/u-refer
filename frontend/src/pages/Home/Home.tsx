fae28ff529aaf7c335a9ad5ce26bc6d2a4ababa6
Stu Lustig <slustig@umass.edu>
2021-10-02 16:52:12 -0400

    update home to use fetch

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { Button, Typography } from '@mui/material'
-import axios from 'axios'
+// import axios from 'axios'
 
 export default function Home() {
   const [fetchedResult, setFetchedResult] = useState<any>(null);
-  const testRequest = async () => {
-    const res = await axios.get('http://localhost:5000/home')
-    setFetchedResult(res.data)
+  // const testRequest = async () => {
+  //   const res = await axios.get('http://localhost:5000/home')
+  //   setFetchedResult(res.data)
+  // }
+  const testRequest = async function() {
+    fetch("/home").then(res => setFetchedResult(res.json()));
   }
   return (
     <>
