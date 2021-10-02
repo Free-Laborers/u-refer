b9d052d9d28a3d5cbc70de13a4e0cd7f97fe0cf5
slustig210 <slustig@umass.edu>
2021-10-02 17:06:44 -0400

    changed fetchedResult to message

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,7 +3,8 @@ import { Button, Typography } from '@mui/material'
 // import axios from 'axios'
 
 export default function Home() {
-  const [fetchedResult, setFetchedResult] = useState<any>(null);
+  const [message, setMessage] = useState<string>("");
+  // const [fetchedResult, setFetchedResult] = useState<any>(null);
   // const testRequest = async () => {
   //   const res = await axios.get('http://localhost:5000/home')
   //   setFetchedResult(res.data)
@@ -11,13 +12,13 @@ export default function Home() {
   const testRequest = async function() {
     fetch("/home")
         .then(res => res.json())
-        .then(setFetchedResult);
+        .then(json => setMessage(json.message));
   }
   return (
     <>
       <Typography variant='h1'>Home</Typography>
       <Button onClick={testRequest}>Fetch</Button>
-      <Typography>Fetched result: {JSON.stringify(fetchedResult)}</Typography>
+      <Typography>Fetched result: {message}</Typography>
     </>
   )
 }
