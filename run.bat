08f002dda3e738fdbb34928de07b318323a1a6d2
Ban B <dbove54@gmail.com>
2021-10-07 19:45:20 -0400

    automated prisma migrate in run.bat and run.sh

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-docker-compose up --build
+docker-compose up --build -d
+docker exec -it urefer-backend npx prisma migrate dev --name init
 docker cp urefer-backend:/app/prisma/migrations ./backend/prisma
