914d859b071fc3cffc270999ebe156704aed088b
Ric Donati <rdonati5645@gmail.com>
2021-10-08 14:58:09 -0400

    Adds factories. Seeing still needs to be done

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,53 +1,22 @@
-import { PrismaClient } from '@prisma/client'
-import faker from 'faker'
-const prisma = new PrismaClient()
+import { createEmployee, createCandidate, createJobPost, createReferral } from "./factories"
 
-// In case we want to use our own job titles
-const possiblePositions = [
-  'Software Engineer',
-  'Software Developer',
-  'DevOps',
-  'Data Analyst',
-  'HR',
-  'Assistant to the Regional Manager',
-]
-
-const createEmployee = async () => {
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
-  const email = faker.internet.email(firstName, lastName, 'ukg')
-  const password = faker.internet.password()
-  // Chooses random position
-  // const position = possiblePositions[Math.floor(Math.random() * possiblePositions.length)]
-  const position = faker.name.jobTitle()
-  const isManager = faker.datatype.boolean()
-
-  prisma.employee.create({
-    data: {
-      email,
-      password,
-      firstName,
-      lastName,
-      position,
-      isManager
-    }
-  })
-}
 
 const main = async () => {
-
-  for(let i = 0; i < 10; ++i){
-    createEmployee()
+  // TODO
+  // Add seed data (using the factories)
+  // Note that certain factories have dependent fields (e.g. to create a job post, you must use the ID of an existing manger for the hiringManagerId)
+  for (let i = 0; i < 10; ++i) {
+    console.log(i)
+    const m = await createEmployee({isManager: true})
+    const e = await createEmployee({isManager: true})
+    for(let i = 0; i < 3; ++i){
+      createJobPost({hiringManagerId: m.id})
+    }
   }
-  
 }
-
 
 main()
   .catch(e => {
     console.error(e)
     process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
   })
