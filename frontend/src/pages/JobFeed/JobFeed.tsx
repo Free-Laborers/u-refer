4fd326b3b6c751d6abe3f1ed366c2ecddce9cca7
Ric Donati <rdonati5645@gmail.com>
2021-10-12 18:45:51 -0400

    temp

--- frontend/src/pages/JobFeed/JobFeed.tsx
+++ frontend/src/pages/JobFeed/JobFeed.tsx
@@ -1,12 +1,14 @@
 import { Typography } from '@mui/material'
 import useAxios from 'axios-hooks'
+// import { JobPost } from '@pris'
+import JobCard from './components/JobCard'
 
 export default function JobFeed() {
   const [{ data, loading, error }, refetch] = useAxios('http://localhost:5000/jobs')
   return (
     <>
       <Typography variant='h1'>Job Feed</Typography>
-      {JSON.stringify(data, undefined, 2)}
+      {data && data.map(job => <JobCard job={job} />)}
     </>
   )
 }
