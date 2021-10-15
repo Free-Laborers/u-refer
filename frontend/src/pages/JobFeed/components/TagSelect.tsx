edac16599be4a28a8966d6a2b21dcb0c728435d2
Ric Donati <rdonati5645@gmail.com>
2021-10-14 22:21:21 -0400

    Improves tag select

--- frontend/src/pages/JobFeed/components/TagSelect.tsx
+++ frontend/src/pages/JobFeed/components/TagSelect.tsx
@@ -1,31 +1,56 @@
-import { Chip, MenuItem, OutlinedInput, Select } from '@mui/material'
+import { Chip, MenuItem, OutlinedInput, Select, SelectChangeEvent, Theme } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 import { Box } from '@mui/system'
 import useAxios from 'axios-hooks'
 import { useState } from 'react'
 
+// Styles dropdown items
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight: personName.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium,
+  }
+}
+
 export default function TagSelect() {
-  // const tags = ['React', 'MUI', 'Another']
+  const theme = useTheme()
   const [{ data }] = useAxios('http://localhost:5000/tags')
   const [selectedTags, setSelectedTags] = useState<string[]>([])
   const tags = data?.tags || []
 
+  const handleDelete = (valToDelete: string) => {
+    const newTags = [...selectedTags].filter(t => t !== valToDelete)
+    console.log(newTags)
+    setSelectedTags(newTags)
+  }
+
+  const handleChange = (event: SelectChangeEvent<typeof selectedTags>) => {
+    const {
+      target: { value },
+    } = event
+    setSelectedTags(
+      // On autofill we get a the stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    )
+  }
+
   return (
     <Select
       size='small'
-      placeholder='Tags'
       multiple
       value={selectedTags}
-      onChange={e => setSelectedTags(e.target.value as string[])}
+      onChange={handleChange}
       input={<OutlinedInput />}
       renderValue={selected => (
         <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
           {selected.map(value => (
-            <Chip key={value} label={value} />
+            // TODO Figure out why handleDelete is not triggered when button is clicked
+            <Chip key={value} label={value} onDelete={() => handleDelete(value)} />
           ))}
         </Box>
-      )}>
-      {tags.map(({name, id}) => (
-        <MenuItem key={id} value={name}>
+      )}
+    >
+      {tags.map(({ name, id }) => (
+        <MenuItem key={name} value={name} style={getStyles(name, selectedTags, theme)}>
           {name}
         </MenuItem>
       ))}
