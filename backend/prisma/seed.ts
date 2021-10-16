97b2bd62ab9246cb8749fcb082585960bc3cab59
sunwoojeong <sunwoojeongofficial@gmail.com>
2021-10-16 14:20:40 -0400

    Clean up ignored files

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,7 +1,13 @@
-import { PrismaClient } from '@prisma/client';
-import { createEmployee, createCandidate, createJobPost, createReferral, addTags } from "./factories"
+import { PrismaClient } from "@prisma/client";
+import {
+  createEmployee,
+  createCandidate,
+  createJobPost,
+  createReferral,
+  addTags,
+} from "./factories";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const clearAllTables = async () => {
   await Promise.all([
@@ -11,22 +17,24 @@ const clearAllTables = async () => {
     prisma.postToTag.deleteMany({}),
     prisma.referral.deleteMany({}),
     prisma.jobPost.deleteMany({}),
-  ])
-}
+  ]);
+};
 
 const main = async () => {
-  await clearAllTables()
-  const manager   = await createEmployee({ isManager: true })
-  const employee  = await createEmployee({ isManager: false })
-  const candidate = await createCandidate()
-  const jobPost   = await createJobPost({ hiringManagerId: manager.id })
-  const referral  = await createReferral({ employeeId: employee.id, jobPostId: jobPost.id, candidateId: candidate.id })
-  addTags(jobPost, ['React', 'Prisma'])
-  
-}
+  await clearAllTables();
+  const manager = await createEmployee({ isManager: true });
+  const employee = await createEmployee({ isManager: false });
+  const candidate = await createCandidate();
+  const jobPost = await createJobPost({ hiringManagerId: manager.id });
+  const referral = await createReferral({
+    employeeId: employee.id,
+    jobPostId: jobPost.id,
+    candidateId: candidate.id,
+  });
+  addTags(jobPost, ["React", "Prisma"]);
+};
 
-main()
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
-  })
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
