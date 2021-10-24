6804bedb3dfbee25ed3906521ac6f247c01f1f5a
sunwoojeong <sunwoojeongofficial@gmail.com>
2021-10-24 00:22:49 -0400

    formatting and bug-fx

--- backend/prisma/factories.ts
+++ backend/prisma/factories.ts
@@ -1,27 +1,35 @@
-import { PrismaClient, Employee, Candidate, JobPost, Referral, Tag, PostToTag } from '@prisma/client'
-import faker from 'faker'
-const prisma = new PrismaClient()
+import {
+  PrismaClient,
+  Employee,
+  Candidate,
+  JobPost,
+  Referral,
+  Tag,
+  PostToTag,
+} from "@prisma/client";
+import faker from "faker";
+const prisma = new PrismaClient();
 
 // Copied from https://medium.com/@KevinBGreene/typescript-modeling-required-fields-with-mapped-types-f7bf17688786
 // Allows you to choose which fields to require from an existing interface
 // Usage: RequireFields<Interface, 'required_key_1, | 'required_key_2' | ...>
 export type RequireFields<T, K extends keyof T> = {
-  [X in Exclude<keyof T, K>]?: T[X]
+  [X in Exclude<keyof T, K>]?: T[X];
 } & {
-  [P in K]-?: T[P]
-}
+  [P in K]-?: T[P];
+};
 
 // In case we want to use our own job titles
 // const possiblePositions = ['Software Engineer', 'Software Developer', 'DevOps', 'Data Analyst', 'HR', 'Assistant to the Regional Manager']
 // const position = possiblePositions[Math.floor(Math.random() * possiblePositions.length)]
 
 export const createEmployee = async (employeeData?: Partial<Employee>) => {
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
-  const email = faker.internet.email(firstName, lastName, 'ukg.com')
-  const password = faker.internet.password()
-  const position = faker.name.jobTitle()
-  const isManager = faker.datatype.boolean()
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email(firstName, lastName, "ukg.com");
+  const password = faker.internet.password();
+  const position = faker.name.jobTitle();
+  const isManager = faker.datatype.boolean();
 
   const defaultData = {
     email,
@@ -30,38 +38,44 @@ export const createEmployee = async (employeeData?: Partial<Employee>) => {
     lastName,
     position,
     isManager,
-  }
+  };
 
-  const res = await prisma.employee.create({ data: Object.assign(defaultData, employeeData) })
+  const res = await prisma.employee.create({
+    data: Object.assign(defaultData, employeeData),
+  });
 
-  return res
-}
+  return res;
+};
 
 export const createCandidate = async (candidateData?: Partial<Candidate>) => {
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
-  const email = faker.internet.email(firstName, lastName, 'ukg.com')
-  const phone = faker.phone.phoneNumber()
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email(firstName, lastName, "ukg.com");
+  const phone = faker.phone.phoneNumber();
 
   const defaultData = {
     email,
     firstName,
     lastName,
     phone,
-  }
+  };
 
-  const res = await prisma.candidate.create({ data: Object.assign(defaultData, candidateData) })
+  const res = await prisma.candidate.create({
+    data: Object.assign(defaultData, candidateData),
+  });
 
-  return res
-}
+  return res;
+};
 
-export const createJobPost = async (jobPostData: RequireFields<JobPost, 'hiringManagerId'>) => {
-  const title = faker.name.jobTitle()
-  const position = faker.name.jobTitle() // Idk what we're expecting this field to be
-  const description = faker.lorem.paragraphs(2)
-  const minYearsExperience = faker.datatype.number(10)
-  const salary = faker.datatype.number(150000)
-  const openings = faker.datatype.number(3) + 1
+export const createJobPost = async (
+  jobPostData: RequireFields<JobPost, "hiringManagerId">
+) => {
+  const title = faker.name.jobTitle();
+  const position = faker.name.jobTitle(); // Idk what we're expecting this field to be
+  const description = faker.lorem.paragraphs(2);
+  const minYearsExperience = faker.datatype.number(10);
+  const salary = faker.datatype.number(150000);
+  const openings = faker.datatype.number(3) + 1;
 
   const defaultData = {
     title,
@@ -70,31 +84,38 @@ export const createJobPost = async (jobPostData: RequireFields<JobPost, 'hiringM
     minYearsExperience,
     salary,
     openings,
-  }
+  };
 
   const res = await prisma.jobPost.create({
     data: Object.assign(defaultData, jobPostData),
-  })
+  });
 
-  return res
-}
+  return res;
+};
 
-export const createReferral = async (referralData: RequireFields<Referral, 'employeeId' | 'candidateId' | 'jobPostId'>) => {
-  const description = faker.lorem.paragraphs()
-  const resumeFilePath = faker.internet.url()
+export const createReferral = async (
+  referralData: RequireFields<
+    Referral,
+    "employeeId" | "candidateId" | "jobPostId"
+  >
+) => {
+  const description = faker.lorem.paragraphs();
+  const resumeFilePath = faker.internet.url();
 
   const defaultData = {
     description,
     resumeFilePath,
-  }
+  };
 
-  const res = await prisma.referral.create({ data: Object.assign(defaultData, referralData) })
+  const res = await prisma.referral.create({
+    data: Object.assign(defaultData, referralData),
+  });
 
-  return res
-}
+  return res;
+};
 
 /**
- * 
+ *
  * @param jobPost Job post object that the tags should be created for
  * @param tags No value -> single random tag. Single string -> single specified tag. Array of strings -> multiple specified tags.
  * @returns PostToTag(s) of the created tag(s)
@@ -116,26 +137,38 @@ export const addTags = async (jobPost: JobPost, tags?: string[] | string) => {
       "Senior",
     ];
     tags = tagOptions[Math.floor(Math.random() * tagOptions.length)];
+    const t =
+      (await prisma.tag.findUnique({ where: { name: tags } })) ??
+      (await prisma.tag.create({ data: { name: tags } }));
+    await prisma.postToTag.create({
+      data: { jobPostId: jobPost.id, tagId: t.id },
+    });
   }
+
   // Specified tags
   else if (Array.isArray(tags)) {
     const ts = await Promise.all(
       tags.map(async (t) => {
         return (
           (await prisma.tag.findUnique({ where: { name: t } })) ??
-          (await prisma.tag.create({ data: {name: t}}))
+          (await prisma.tag.create({ data: { name: t } }))
         );
       })
     );
 
     ts.map(
-      async (t) => await prisma.postToTag.create({data: { jobPostId: jobPost.id, tagId: t.id }})
+      async (t) =>
+        await prisma.postToTag.create({
+          data: { jobPostId: jobPost.id, tagId: t.id },
+        })
     );
   } else {
     // Specified tag
     const t =
       (await prisma.tag.findUnique({ where: { name: tags } })) ??
-      (await prisma.tag.create({ data: {name: tags}}))
-      await prisma.postToTag.create({data: { jobPostId: jobPost.id, tagId: t.id }})
+      (await prisma.tag.create({ data: { name: tags } }));
+    await prisma.postToTag.create({
+      data: { jobPostId: jobPost.id, tagId: t.id },
+    });
   }
 };
--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,17 +1,23 @@
-import { PrismaClient } from '@prisma/client'
-import { createEmployee, createCandidate, createJobPost, createReferral, addTags } from './factories'
+import { PrismaClient } from "@prisma/client";
+import {
+  createEmployee,
+  createCandidate,
+  createJobPost,
+  createReferral,
+  addTags,
+} from "./factories";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const clearAllTables = async () => {
   await prisma.postToTag
     .deleteMany({})
-    .then(_ => prisma.tag.deleteMany({}))
-    .then(_ => prisma.referral.deleteMany({}))
-    .then(_ => prisma.jobPost.deleteMany({}))
-    .then(_ => prisma.candidate.deleteMany({}))
-    .then(_ => prisma.employee.deleteMany({}))
-}
+    .then((_) => prisma.tag.deleteMany({}))
+    .then((_) => prisma.referral.deleteMany({}))
+    .then((_) => prisma.jobPost.deleteMany({}))
+    .then((_) => prisma.candidate.deleteMany({}))
+    .then((_) => prisma.employee.deleteMany({}));
+};
 
 const main = async () => {
   await clearAllTables();
@@ -42,7 +48,7 @@ const main = async () => {
   await addTags(jp4, ["PostgreSQL"]);
 };
 
-main().catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
