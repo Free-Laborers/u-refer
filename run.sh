4b4e069e3056c869242aa2d8e405d9cdc1c49f03
slustig210 <slustig@umass.edu>
2021-10-27 21:53:42 -0400

    removed unnecessary line from run scripts

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ cd ..
 docker-compose up --build -d
 docker exec -it urefer-backend npx prisma migrate dev --name init --skip-seed
 docker exec -it urefer-backend npx prisma db seed
-docker cp urefer-backend:/app/prisma/migrations ./backend/prisma
+# docker cp urefer-backend:/app/prisma/migrations ./backend/prisma
