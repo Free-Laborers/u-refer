6e20388480eb32dc6015a0acdf76aeab855924e1
slustig210 <slustig@umass.edu>
2021-11-02 23:29:52 -0400

    fixed typos in shell scripts

--- migrate_reset.sh
+++ migrate_reset.sh
@@ -1,4 +1,4 @@
 docker-compose up -d db
-export DATABASE_URL='postgresql://postgres:docker@urefer-db:5432/urefer?schema=public'
+export DATABASE_URL='postgresql://postgres:docker@localhost:5432/urefer?schema=public'
 cd ./backend
 npx prisma migrate reset --skip-seed # seeding will be done by run...?
