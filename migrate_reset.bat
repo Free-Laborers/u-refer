c0ca59b13338ba697fed6b452166d166e79f0534
slustig210 <slustig@umass.edu>
2021-11-09 15:44:17 -0500

    missed 1 change in the sh, changed capitalization to match

--- migrate_reset.bat
+++ migrate_reset.bat
@@ -3,7 +3,7 @@
 @REM drops the database and creates a new one before applying all migrations.
 
 CALL docker-compose up -d db
-set DATABASE_URL=postgresql://postgres:docker@localhost:5433/urefer?schema=public
+SET DATABASE_URL=postgresql://postgres:docker@localhost:5433/urefer?schema=public
 IF DEFINED UREFER_DB_PORT SET DATABASE_URL=postgresql://postgres:docker@localhost:%UREFER_DB_PORT%/urefer?schema=public
-cd .\backend
+CD .\backend
 CALL npx prisma migrate reset --skip-seed @REM seeding will be done by run...?
--- migrate_reset.sh
+++ migrate_reset.sh
@@ -1,4 +1,4 @@
 docker-compose up -d db
-export DATABASE_URL='postgresql://postgres:docker@localhost:5433/urefer?schema=public'
+export DATABASE_URL='postgresql://postgres:docker@localhost:${UREFER_DB_PORT:-5433}/urefer?schema=public'
 cd ./backend
 npx prisma migrate reset --skip-seed # seeding will be done by run...?
