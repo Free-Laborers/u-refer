ee111a26b38f6e6ae0b28311ceb42e8643649f16
sunwoojeong <sunwoojeongofficial@gmail.com>
2021-11-13 17:42:47 -0500

    file upload on frontend

--- frontend/src/components/ReferralCreationModal/pages/ResumePage.tsx
+++ frontend/src/components/ReferralCreationModal/pages/ResumePage.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import { Box } from '@mui/material';
+import React, { useState } from 'react'
 import Dropzone from 'react-dropzone'
 
 export default function ResumePage() {
+  const [file, setFile] = useState<any>();
+
+  const handleDrop = acceptedFiles =>{
+    setFile(acceptedFiles[0])
+  }
   return (
-    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-      {({getRootProps, getInputProps}) => (
-        <section>
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
-        </section>
-      )}
-    </Dropzone>
-  )
+    <div className="App">
+      <div style={{paddingTop: "20px"}}>
+        <div><strong>PDF only</strong></div>
+        <Dropzone onDrop={handleDrop} accept= "application/pdf" maxSize={30000000} multiple={false}>
+          {({ getRootProps, getInputProps }) => (
+            <div {...getRootProps({ className: "dropzone" })}>
+              <input {...getInputProps()} />
+                <Box
+                  sx={{
+                    width: '100%',
+                    height: 300,
+                    border: 1
+                  }}
+                >
+                  Drag'n'drop your resume, or click to select files
+                </Box>
+            </div>
+          )}
+        </Dropzone>
+      </div>
+      <div>
+        <strong>File: </strong> {file? file.name: ""}
+      </div>
+    </div>
+  );
 }
+
+
