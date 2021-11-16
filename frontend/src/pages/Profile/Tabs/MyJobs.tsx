95bc83d7f7f19451a99e197a8b7f29e3322df983
Dan B <dbove54@gmail.com>
2021-11-16 01:44:42 -0500

    Creating the myJobs Tab for the profile page

---
+++ frontend/src/pages/Profile/Tabs/MyJobs.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+//@ts-ignore
+import { JobPost } from "../../../../../backend/node_modules/prisma/prisma-client";
+import useAxios from "axios-hooks";
+import { Typography } from "@mui/material";
+import DataTable from "../components/DataTable"
+
+interface JobFeedResponseType {
+  data: JobPost[];
+}
+export default function MyJobs(props) {
+  const [userData, setUserData] = useState({
+    id: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    position: "",
+    isManager: "",
+  });
+
+  const [{ data }] = useAxios<JobFeedResponseType>({
+    url: "/jobPost/manager",
+    headers: {
+      Authorization: localStorage.getItem("authorization"),
+    },
+  });
+
+  useEffect(() => {
+    async function getData() {
+      const auth = localStorage.getItem("authorization");
+
+      if (auth) {
+        try {
+          const response = await axios("/employee/profile", {
+            method: "GET",
+            headers: {
+              Authorization: localStorage.getItem("authorization"),
+            },
+          });
+          const json = await response.data;
+          return setUserData(json.user);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+
+    getData();
+  }, []);
+  return (
+    <>
+      <Typography>This is where {userData.firstName} {userData.lastName}'s jobs will go</Typography>
+      <Typography>{userData.id}</Typography>
+      <Typography>{JSON.stringify(data)}</Typography>
+      <DataTable/>
+    </>
+  );
+}
