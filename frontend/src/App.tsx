1c8a94324657d3e8e7e7a4071f754c8f0fe1ae49
slustig210 <slustig@umass.edu>
2021-11-16 16:23:42 -0500

    fixed an issue, added 404 type route NoMatch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/system'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // import Navbar from './components/Navbar'
 import { JobFeedFilterContextProvider } from './contexts/JobFeedFilterContext'
 import Home from './pages/Home'
@@ -10,25 +10,27 @@ import Profile from './pages/Profile'
 import { AuthProvider } from './hooks/useAuth'
 import PrivateRoute from './components/Routes/PrivateRoute'
 import RestrictedRoute from './components/Routes/RestrictedRoute'
+import NoMatch from './pages/NoMatch'
 
 const App = () => {
   return (
-    <JobFeedFilterContextProvider>
-      <Router>
-        <Box sx={{ marginTop: '64px' }}>
-          <Switch>
-            <AuthProvider>
-              <RestrictedRoute component={Login} exact path='/login' />
-              <PrivateRoute component={Home} exact path='/' />
-              {/* <PrivateRoute component={Home} exact path='/refer' /> */}
-              <PrivateRoute component={JobFeed} exact path='/jobs' />
-              <PrivateRoute component={Listing} exact path='/jobs/create' />
-              <PrivateRoute component={Profile} exact path='/profile' />
-            </AuthProvider>
-          </Switch>
-        </Box>
-      </Router>
-    </JobFeedFilterContextProvider>
+    <AuthProvider>
+      <JobFeedFilterContextProvider>
+        <Router>
+          <Box sx={{ marginTop: '64px' }}>
+            <Switch>
+                <RestrictedRoute component={Login} exact path='/login' />
+                <PrivateRoute component={Home} exact path='/' />
+                {/* <PrivateRoute component={Home} exact path='/refer' /> */}
+                <PrivateRoute component={JobFeed} exact path='/jobs' />
+                <PrivateRoute component={Listing} exact path='/jobs/create' />
+                <PrivateRoute component={Profile} exact path='/profile' />
+                <Route component={NoMatch} /> {/* No match => page not found */}
+            </Switch>
+          </Box>
+        </Router>
+      </JobFeedFilterContextProvider>
+    </AuthProvider>
   )
 }
 
