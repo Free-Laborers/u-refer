64c91f5da66b9c5ad1e782975021140512b00079
Kriti Singh <kritisingh@umass.edu>
2021-11-19 10:08:00 -0500

    added grid template for the page and empty component files

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,9 +1,14 @@
 import {useEffect, useState} from 'react';
 import Tab from '@mui/material/Tab'
-import {Tabs} from '@mui/material'
+import {Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Tabs} from '@mui/material'
 import axios from 'axios';
 import MyReferrals from './Tabs/MyReferrals';
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 //https://stackoverflow.com/questions/66012476/how-to-show-hide-mui-tabs-based-on-a-condition-and-maintain-the-right-tab-index
+
+
+const theme = createTheme();
 
 const Profile = () => {
 
@@ -38,6 +43,30 @@ const Profile = () => {
     getData();
   }, []);
 
+  const [sort, sortBy] = React.useState('');
+  const handleChange = (event: SelectChangeEvent) => {
+    sortBy(event.target.value as string);
+  };
+  const renderMenu = (
+    <Box mb={4} sx={{ minWidth: 120 , m: 2}}>
+      <FormControl fullWidth>
+        <InputLabel>Sort By:</InputLabel>
+        <Select
+          value={sort}
+          label="Sort By:"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Date: Most Recent</MenuItem>
+          <MenuItem value={2}>Date: Oldest</MenuItem>
+          <MenuItem value={3}>Open</MenuItem>
+          <MenuItem value={4}>Closed</MenuItem>
+          {/* <MenuItem value={5}>Number of Applicants: Low to High</MenuItem>
+          <MenuItem value={6}>Number of Applicants: High to Low</MenuItem> */}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+
 
     const [value, setValue] = useState(-1);
     const handleTab = (event, val) => {
@@ -45,15 +74,30 @@ const Profile = () => {
     }
     const isAManager = userData.isManager;
     return (
-        <>
-           <Tabs value={value} onChange={handleTab}>
-            <Tab label="My Referrals" value={0} />
+        <Box height={"calc(100vh - 64px)"} >
+          <Box
+            m={2}
+            display="grid"
+            height="100%"
+            gridTemplateRows="auto 1fr auto"
+            gridTemplateColumns="1fr 1fr"
+            gridTemplateAreas={`"referList     referCard"
+                                "pagination   .       "`}                  
+            columnGap={2}
+          >
+          <Box sx={{ gridArea: "referList" }} overflow="auto">
+          <Tabs value={value} onChange={handleTab} centered>
+            <Tab label="My Referrals" value={0} color='red' />
             { isAManager && <Tab label="My Positions" value={1} />}
-           </Tabs>
+          </Tabs>
+          {renderMenu}
            <TabPanel value={value} index={0}><MyReferrals userData/></TabPanel>
            <TabPanel value={value} index={1}>This is where {userData.firstName} {userData.lastName} positions will show</TabPanel>
-          
-        </>
+          </Box>
+          <Box sx={{ gridArea: "referCard" }} overflow="auto">
+          </Box>
+          </Box>
+        </Box>
     )
 }
 
