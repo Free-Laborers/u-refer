4a659a9d605ba6193d2c0221272bc8d8a3b62c05
Son Dam <sdam@umass.edu>
2021-12-01 19:35:03 -0500

    Referral Preview Card

--- frontend/src/pages/Profile/components/ReferralPreviewCard.tsx
+++ frontend/src/pages/Profile/components/ReferralPreviewCard.tsx
@@ -1,8 +1,46 @@
-import { Paper } from "@mui/material";
+import { Chip, Paper, Stack, Typography } from "@mui/material";
+//@ts-ignore
+import { Referral } from "../../../../../backend/node_modules/prisma/prisma-client";
+import ValueWithLabel from "../../../components/ValueWithLabel";
 
-export default function ReferralPreviewCard() {
+interface ReferralPreviewCardProps {
+  referral: Referral;
+  onClick;
+}
+
+export default function ReferralPreviewCard(props: ReferralPreviewCardProps) {
+  const {referral, onClick} = props;
   return(
-    <Paper sx={{ p: 2, height: "100%" }}>
-  </Paper>
+    <Paper onClick = {onClick} sx={{ mb:2, p: 2, cursor: "pointer" }}>
+      <Typography>
+        {referral.id}
+      </Typography>
+      <Stack direction = "row" spacing = {0.5} mb = {2}>
+        {
+          //@ts-ignore
+          referral.Referral.map(r =>(
+            <Chip
+            label = {r.Tag.name}
+            variant = "filled"
+            color = "default"
+            />
+          ))
+
+        }
+
+      </Stack>
+      <ValueWithLabel
+        sx ={{
+          display: "-webkit-box",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          whiteSpace: "initial",
+          WebkitBoxOrient: "vertical",
+          WebkitLineClamp: 3,
+        }} 
+        label = "Description"
+        value = {referral?.description}
+      />
+    </Paper>
   );
 }
