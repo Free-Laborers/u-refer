5dfe0d11d841e0afc3181555b00a5619fec06cc1
Dan B <dbove54@gmail.com>
2021-12-09 22:18:35 -0500

    Handle what shows on the button and redirects

--- frontend/src/pages/JobFeed/components/JobCard.tsx
+++ frontend/src/pages/JobFeed/components/JobCard.tsx
@@ -5,13 +5,24 @@ import { Box } from "@mui/system";
 import { JobPost } from "../../../../../backend/node_modules/prisma/prisma-client";
 import ValueWithLabel from "../../../components/ValueWithLabel";
 import ReferralCreationModal from "../../../components/ReferralCreationModal";
+import useAuth from "../../../hooks/useAuth";
+import { useHistory } from "react-router";
 
 interface JobCardProps {
   job: JobPost;
 }
 
 export default function JobCard(props: JobCardProps) {
+  const { user } = useAuth();
   const { job } = props;
+  const history = useHistory();
+  function handleButtonPressed() {
+    if (job.hiringManagerId === user?.id)
+      //*****Change this to whatever Stu/Ric/Jake are making******
+      history.push("/profile");
+    //*****Change this to whatever Stu/Ric/Jake are making******
+    else setReferralCreationModalOpen(true);
+  }
   const [referralCreationModalOpen, setReferralCreationModalOpen] =
     useState(false);
   const JobCardContent = job ? (
@@ -25,18 +36,14 @@ export default function JobCard(props: JobCardProps) {
             {new Date(job?.createdDate).toLocaleDateString()}
           </Typography>
         </Box>
-        <Stack direction="row" spacing={0.5} mb = {2}>
+        <Stack direction="row" spacing={0.5} mb={2}>
           {
             // @ts-ignore
-            job.PostToTag.map(ptt => (
-              <Chip
-              label = {ptt.Tag.name}
-              variant ="filled"
-              color='default' 
-              />
+            job.PostToTag.map((ptt) => (
+              <Chip label={ptt.Tag.name} variant="filled" color="default" />
             ))
           }
-      </Stack>
+        </Stack>
         <ValueWithLabel label="Description" value={job?.description} />
         <ValueWithLabel
           label="Salary"
@@ -45,12 +52,8 @@ export default function JobCard(props: JobCardProps) {
         <ValueWithLabel label="Experience" value={job?.minYearsExperience} />
         <ValueWithLabel label="Openings" value={job?.openings} />
       </Box>
-      <Button
-        onClick={() => setReferralCreationModalOpen(true)}
-        variant="contained"
-        color="primary"
-      >
-        Refer
+      <Button onClick={handleButtonPressed} variant="contained" color="primary">
+        {job.hiringManagerId === user?.id ? "View Referrals" : "Refer"}
       </Button>
     </Box>
   ) : (
