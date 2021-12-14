a4d548c20ec063e55cdd05e537bcd02481551ac0
slustig210 <slustig@umass.edu>
2021-12-13 20:15:12 -0500

    changed home to redirect to jobs

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,20 +1,24 @@
-import { Typography } from '@mui/material'
-import useAuth from '../../hooks/useAuth';
+// import { Typography } from '@mui/material'
+// import useAuth from '../../hooks/useAuth';
+
+import { Redirect } from "react-router";
 
 export default function Home() {
-  const { user } = useAuth();
+  return <Redirect to="/jobs" />
 
-  return (
-    <>
-      <Typography variant='h1'>Welcome home!</Typography>
-      <Typography data-testid='paragraph'>
-        Data of logged in user: <br/>
-        Email: {user?.email} <br/>
-        First name: {user?.firstName} <br/>
-        Last name: {user?.lastName} <br/>
-        Position: {user?.position} <br/>
-        Is manager: {String(user?.isManager)}
-      </Typography>
-    </>
-  )
+  // const { user } = useAuth();
+
+  // return (
+  //   <>
+  //     <Typography variant='h1'>Welcome home!</Typography>
+  //     <Typography data-testid='paragraph'>
+  //       Data of logged in user: <br/>
+  //       Email: {user?.email} <br/>
+  //       First name: {user?.firstName} <br/>
+  //       Last name: {user?.lastName} <br/>
+  //       Position: {user?.position} <br/>
+  //       Is manager: {String(user?.isManager)}
+  //     </Typography>
+  //   </>
+  // )
 }
