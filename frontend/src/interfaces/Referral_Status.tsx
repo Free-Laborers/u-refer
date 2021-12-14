c45f097fd45eee36751b56543bc76e3618f06e87
slustig210 <slustig@umass.edu>
2021-12-13 21:49:49 -0500

    removed unused interfaces

--- frontend/src/interfaces/Referral_Status.tsx
+++
@@ -1,9 +0,0 @@
-export const Referral_Status = {
-    OPEN: 'OPEN',
-    CONTACTED: 'CONTACTED',
-    REJECTED: 'REJECTED',
-    HIRED: 'HIRED',
-    DELETED: 'DELETED'
-  };
-  
-  export type Referral_Status = (typeof Referral_Status)[keyof typeof Referral_Status]
